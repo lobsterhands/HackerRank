33aa4d4bf3607bc58cb7b32f3439381c2d0687c9
Lyle <denmanlp@gmail.com>
2016-05-14 13:28:46 -0500

    Add get input

---
+++ Day01/day1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    char s[] = "HackerRank ";
+
+
+    // Declare second integer, double, and String variables.
+    int userI;
+    double userD;
+    char userS[99];
+
+    // Read and save an integer, double, and String to your variables.
+    scanf("%d\n", &userI);
+    scanf("%lf\n", &userD);
+    scanf("%[^\n]", userS);
+
+    // Print the sum of both integer variables on a new line.
+
+    // Print the sum of the double variables on a new line.
+
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+
+
+
+    return 0;
+}
