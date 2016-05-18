ab6e81f1cd2cb3f6ac36078fd6bc345615148578
Lyle <denmanlp@gmail.com>
2016-05-18 11:57:11 -0500

    day03 complete

---
+++ Day03/day3.c
@@ -0,0 +1,23 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main() {
+    int N;
+    scanf("%d",&N);
+
+    if (N % 2 != 0) {
+        printf("Weird\n");
+    } else if ( (N >= 2 && N <= 5) || N > 20 )
+    {
+        printf("Not Weird\n");
+    } else {
+        printf("Weird\n");
+    }
+
+    return 0;
+}
